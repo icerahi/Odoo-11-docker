0c8173eb28f015cd110f5166864753bbaf62bae8
rahi <zanjarwhite@gmail.com>
2020-02-06 11:48:40 +0600

    initailize

---
+++ addons/RahiApp/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+{
+'name': 'Rahi App',
+'version': '1.0',
+'category': 'Tools',
+'sequence': 1,
+'summary': 'Just for learning purpose.',
+'website': 'http://www.fb.com/icerahi',
+'depends': [
+    'base',
+],
+'data': [
+'views/view.xml',
+
+],
+'installable': True,
+'auto_install': False,
+'application':True
+}
+
